66305441b9d97dbd98ee6b827772a61cbf433805
Suheon Song <ssheon0812@naver.com>
2023-03-05 22:17:29 +0900

    feat: pc 크롤러 수정

---
+++ test.py
@@ -0,0 +1,42 @@
+import requests, zipfile, io
+r = requests.get('https://d26jfubr2fa7sp.cloudfront.net/Musics.zip')
+z = zipfile.ZipFile(io.BytesIO(r.content))
+z.extractall("./")
+
+tj_dic = {}
+
+f = open("musicbook_TJ.txt", "rt")
+while True:
+    line = f.readline()
+    if line == '' :
+        break
+    s = line.split(sep='^')
+    if s :
+        keyword = s[0] + " " + s[1]
+        number = s[2]
+        tj_dic[number] = keyword
+f.close()
+
+url_number_list = []
+
+f = open("youtube_Url.txt", "rt")
+while True:
+    line = f.readline()
+    if line == '':
+        break
+    s = line.split(sep='^')
+    if s:
+        number = s[0]
+        url_number_list.append(number)
+f.close()
+
+keyword = []
+
+for number, title in tj_dic.items():
+    if number not in url_number_list:
+        keyword.append(number+"^"+title)
+
+f = open("url_keyword.txt", 'w')
+for k in keyword:
+    f.write(k + "\n")
+f.close()
--- youtubecrawler_headless.py
+++ youtubecrawler_headless.py
@@ -1,49 +1,41 @@
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
 from bs4 import BeautifulSoup
 import time
 import string
 
 # 최신 크롬 드라이버 사용하도록 세팅: 현재 OS에 설치된 크롬 브라우저 버전에 맞게 cache에 드라이버 설치
 from selenium.webdriver.chrome.service import Service
-service = Service(ChromeDriverManager().install())
+# service = Service(ChromeDriverManager().install())
 options = webdriver.ChromeOptions()
 options.add_argument('headless')
 options.add_argument('window-size=1920x1080')
 options.add_argument("disable-gpu")
 
 
-f = open("musicbook_TJ.txt", "r")
+f = open("url_keyword.txt", "r")
 strings = f.readlines()
 f.close()
 f = open("youtube_Url.txt", "a")
 f2 = open("fail.txt", "a")
 
 
-for info in strings:
-    keyword = ""
-    number = "" 
-    count = 0
-    # 문자열에서 키워드와 번호 파싱
-    for e in info:
-        if (e == '^'): 
-            count += 1
-            if (count == 1): 
-                keyword += " "
-            elif (count == 3):
-                break
-            continue
-        else:
-            if (count == 2):
-                number += e
-            else:
-                keyword += e
-    driver = webdriver.Chrome(service=service, options=options)
+for s in strings:
+    search = s.split(sep='^')
+    if not search:
+        continue
+    number = search[0]
+    keyword = search[1]
+    
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
     # 검색시 특수문자가 있으면 검색이 안되기 때문에 특수문자 제거
     for character in string.punctuation:
         keyword = keyword.replace(character, ' ')
     # 검색 키워드 설정: 키워드 내 띄어쓰기는 URL에서 '+'로 표시되기 때문에 이에 맞게 변환
     SEARCH_KEYWORD = keyword.replace(' ', '+')
+    if (SEARCH_KEYWORD == '\n'):
+        break
     # 스크래핑 할 URL 세팅
     URL = "https://www.youtube.com/results?search_query=" + SEARCH_KEYWORD
     # 크롬 드라이버를 통해 지정한 URL의 웹 페이지 오픈
@@ -59,8 +51,8 @@ for info in strings:
     driver.quit()
     # 크롤링 실패시 fail.txt에 저장
     if (not content_total_link):
-        f2.write(info)
-        print(info + "크롤링 실패!!!")
+        f2.write(keyword)
+        print(keyword + "크롤링 실패!!!")
         continue
     # 반복되는 url 문자열 제거
     url = str(content_total_link[0]).replace('https://youtube.com/watch?v=','')
